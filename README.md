2fd877172c5db4115eeff10ecc91524621d6aace
Weicong Peng <pengweicong@bytedance.com>
2023-09-26 00:19:47 +0800

    update

--- README.md
+++ README.md
@@ -2,9 +2,9 @@
 
 ## Getting Started
 
-1. `npm i` to install dependancies
-2. `npm start` to start running the fast development mode Webpack build process that bundle files into the `dist` folder
-3. `npm i --save-dev <package_name>` to install new packages
+1. `pnpm i` to install dependancies
+2. `pnpm start` to start running the fast development mode Webpack build process that bundle files into the `dist` folder
+3. `pnpm i --save-dev <package_name>` to install new packages
 
 ## Loading The Chrome Extension
 
@@ -22,7 +22,7 @@
 
 # Production Build
 
-1. `npm run build` to generate a minimized production build in the `dist` folder
+1. `pnpm run build` to generate a minimized production build in the `dist` folder
 2. ZIP the entire `dist` folder (e.g. `dist.zip`)
 3. Publish the ZIP file on the Chrome Web Store Developer Dashboard!
 
--- webpack.common.js
+++ webpack.common.js
@@ -1,34 +1,36 @@
-const path = require('path');
-const CopyPlugin = require('copy-webpack-plugin');
-const HtmlPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
+const HtmlPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+const TITLE = "React Chrome Extension";
 
 module.exports = {
   entry: {
-    popup: path.resolve('src/popup/popup.tsx'),
-    options: path.resolve('src/options/options.tsx'),
-    background: path.resolve('src/background/background.ts'),
-    contentScript: path.resolve('src/contentScript/contentScript.ts'),
+    popup: path.resolve("src/popup/popup.tsx"),
+    options: path.resolve("src/options/options.tsx"),
+    background: path.resolve("src/background/background.ts"),
+    contentScript: path.resolve("src/contentScript/contentScript.ts"),
   },
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        use: 'ts-loader',
+        use: "ts-loader",
         exclude: /node_modules/,
       },
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        use: ["style-loader", "css-loader"],
       },
       {
         test: /\.(jpg|jpeg|png|woff|woff2|eot|ttf|svg)$/,
-        type: 'asset/resource'
-      }
-    ]
+        type: "asset/resource",
+      },
+    ],
   },
   resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
+    extensions: [".tsx", ".ts", ".js"],
   },
   plugins: [
     new CleanWebpackPlugin({
@@ -37,31 +39,29 @@ module.exports = {
     new CopyPlugin({
       patterns: [
         {
-          from: path.resolve('src/static'),
-          to: path.resolve('dist'),
-        }
-      ]
+          from: path.resolve("src/static"),
+          to: path.resolve("dist"),
+        },
+      ],
     }),
-    ...getHtmlPlugins([
-      'popup',
-      'options'
-    ]),
+    ...getHtmlPlugins(["popup", "options"]),
   ],
   output: {
-    filename: '[name].js',
-    path: path.resolve('dist'),
+    filename: "[name].js",
+    path: path.resolve("dist"),
   },
   optimization: {
-    splitChunks: {
-      chunks: 'all',
-    },
-  }
-}
+    splitChunks: false,
+  },
+};
 
 function getHtmlPlugins(chunks) {
-  return chunks.map(chunk => new HtmlPlugin({
-    title: 'React Extension',
-    filename: `${chunk}.html`,
-    chunks: [chunk],
-  }))
+  return chunks.map(
+    (chunk) =>
+      new HtmlPlugin({
+        title: TITLE,
+        filename: `${chunk}.html`,
+        chunks: [chunk],
+      })
+  );
 }
